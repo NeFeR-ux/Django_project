269bb097007dac1c3de271d4a361b89995d207e2
Marat <ed134759@gmail.com>
2026-02-25 21:12:43 +0300

    second task

--- project/pages/models.py
+++ project/pages/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        # Суммарный рейтинг каждой статьи автора умножается на 3
+        post_rating = sum(post.rating for post in Post.objects.filter(author=self)) * 3
+
+        # Суммарный рейтинг всех комментариев автора
+        author_comments_rating = sum(comment.rating for comment in Comment.objects.filter(user=self.user))
+
+        # Суммарный рейтинг всех комментариев к статьям автора
+        post_comments_rating = sum(
+            comment.rating
+            for post in Post.objects.filter(author=self)
+            for comment in Comment.objects.filter(post=post)
+        )
+
+        self.rating = post_rating + author_comments_rating + post_comments_rating
+        self.save()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    ARTICLE = 'AR'
+    NEWS = 'NW'
+    POST_TYPES = [
+        (ARTICLE, 'Статья'),
+        (NEWS, 'Новость'),
+    ]
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=2, choices=POST_TYPES, default=ARTICLE)
+    created_at = models.DateTimeField(default=timezone.now)
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        preview_text = self.text[:124]
+        if len(self.text) > 124:
+            preview_text += '...'
+        return preview_text
+
+    def __str__(self):
+        return self.title
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.category.name}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def __str__(self):
+        return f"Комментарий от {self.user.username} к {self.post.title}"
